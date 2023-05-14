04e13c795a43a5cbb535c82d2e7c1a24fb5f2af9
vainRain <jwbin9038@gmail.com>
2023-05-14 17:01:02 +0800

    finish bind

---
+++ server.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+int main(int argc, char *argv[]) {
+    int serv_sock = socket(PF_INET, SOCK_STREAM, 0);
+    
+    struct sockaddr_in serv_addr;
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port = htons(atoi(argv[1]));
+    bind(serv_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+    
+    return 0;
+}
